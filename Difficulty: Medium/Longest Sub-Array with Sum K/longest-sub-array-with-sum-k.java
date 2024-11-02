9104e0f2a5c7c08af9a2952539d5536fc3f7e55f
Utkarsh Prakash Joshilkar <utkarshjoshilkar@icloud.com>
2024-11-02 09:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution();
+            int ans =
+                ob.lenOfLongestSubarr(nums, k); // Call the function and store result
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int lenOfLongestSubarr(int[] arr, int k) {
+        int n = arr.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = arr[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += arr[right];
+        }
+
+        return maxLen;
+    }
+}
