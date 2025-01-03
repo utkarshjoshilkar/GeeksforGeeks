b9e9078f09d8bc052caf51b3e6df1718ae134336
Utkarsh Prakash Joshilkar <utkarshjoshilkar@icloud.com>
2025-01-03 10:42:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.java
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        next = prev = null;
+    }
+}
+
+class DLinkedList {
+
+    Node newNode(Node head, int data) {
+        Node n = new Node(data);
+        if (head == null) {
+            head = n;
+            return head;
+        }
+        head.next = n;
+        n.prev = head;
+        head = n;
+        return head;
+    }
+
+    void printList(Node node) {
+        Node temp = node;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        while (temp.prev != null) {
+            temp = temp.prev;
+        }
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        DLinkedList DLL = new DLinkedList();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t > 0) {
+            Node temp;
+            Node head = null;
+            Node root = null;
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+
+            for (int i = 0; i < n; i++) {
+                int x = Integer.parseInt(str[i]);
+                head = DLL.newNode(head, x);
+                if (root == null) root = head;
+            }
+            head = root;
+
+            String str2[] = read.readLine().trim().split(" ");
+            int pos = Integer.parseInt(str2[0]);
+            int data = Integer.parseInt(str2[1]);
+
+            Solution g = new Solution();
+            head = g.addNode(head, pos, data);
+
+            DLL.printList(head);
+            while (head.next != null) {
+                temp = head;
+                head = head.next;
+            }
+            t--;
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Structure of Doubly Linked List
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}*/
+
+class Solution {
+    // Function to insert a new node at given position in doubly linked list.
+    Node addNode(Node head, int p, int x) {
+        Node current = head ;
+        Node newNode = new Node(x);
+        int counter = 0;
+        if(current==null)return newNode;
+        
+        while(current!= null && counter<p){
+            current = current.next;
+            counter++;
+        }
+        if(current!=null){
+            newNode.next = current.next;
+            if(current.next!=null){
+                current.next.prev = current;
+            }
+            newNode.prev = current;
+            current.next = newNode;
+        }
+        else{
+            current = newNode;
+        }
+                        
+        return head;
+    }
+}
